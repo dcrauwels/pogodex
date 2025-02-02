01a63d32a03560d25bd80e1a6982970659159253
dcrauwels <d.crauwels@gmail.com>
2025-02-02 11:31:51 +0100

    Changed up the replcli.go structure a bit to implement pointers to a REPL value that is modified by adding commands.

--- main.go
+++ main.go
@@ -5,5 +5,6 @@ import (
 )
 
 func main() {
-	replcli.ReplCLI()
+	r := replcli.NewREPL()
+	r.ReplCLI()
 }
--- replcli/replcli.go
+++ replcli/replcli.go
@@ -15,8 +15,28 @@ type cliCommand struct {
 	callback    func() error
 }
 
+// define struct for REPL and associated function
+type REPL struct {
+	commands map[string]cliCommand
+}
+
+func NewREPL() *REPL {
+	return &REPL{
+		commands: make(map[string]cliCommand),
+	}
+}
+
+// add a command to the REPL, for use in the ReplCLI function to not have this huge definition at the start
+func (r *REPL) RegisterCommand(name string, description string, callback func() error) {
+	r.commands[name] = cliCommand{
+		name:        name,
+		description: description,
+		callback:    callback,
+	}
+}
+
 // first command: exit pokedex
-func commandExit() error {
+func (r *REPL) commandExit() error {
 	fmt.Println("Closing the Pokedex... Goodbye!")
 
 	// any cleanup functions (that might produce errors) go here
@@ -26,48 +46,51 @@ func commandExit() error {
 }
 
 // second command: print help
-func commandHelp(m map[string]cliCommand) error {
+func (r *REPL) commandHelp() error {
 	// header fluff
-	fmt.Println("Welcome to the Pokedex!\nUsage:\n")
+	fmt.Println("Welcome to the Pokedex!\nUsage:")
 
 	// sanity check
-	if len(m) < 1 {
+	if len(r.commands) < 1 {
 		return fmt.Errorf("no commands are implemented")
 	}
 
 	// loop over all commands
-	for _, command := range m {
-		fmt.Fprintln("%s: %s", command.name, command.description)
+	for _, command := range r.commands {
+		fmt.Printf("%s: %s\n", command.name, command.description)
 	}
 	return nil
 }
 
 // main function: open a CLI that loops until interrupt or commandExit() is called
-func ReplCLI() {
-	commands := map[string]cliCommand{
-		"exit": {
-			name:        "exit",
-			description: "Exit the Pokedex",
-			callback:    commandExit,
-		},
-		"help": {
-			name:        "help",
-			description: "Prints a help message",
-			callback:    commandHelp,
-		},
-	}
+func (r *REPL) ReplCLI() error {
+	// register commands here
+	r.RegisterCommand("exit", "Exit the Pokedex", r.commandExit)
+	r.RegisterCommand("help", "Prints this help message", r.commandHelp)
 
 	s := bufio.NewScanner(os.Stdin)
 
 	for {
 		fmt.Print("Pokedex > ")
+
+		// take input and clean it
 		s.Scan()
 		input := s.Text()
 		cleanedInput := stringutils.CleanInput(input)
 		firstInput := cleanedInput[0]
-		if command, ok := commands[firstInput]; ok {
-			command.callback()
+
+		//check if command in r.commands
+		command, ok := r.commands[firstInput]
+		if !ok {
+			fmt.Errorf("Unknown command: %s", firstInput)
 		}
+
+		// try command, raise error if an issue arises
+		if err := command.callback(); err != nil {
+			fmt.Errorf("Error executing command: %w", err)
+		}
+
+		command.callback()
 		fmt.Printf("Your command was: %s\n", firstInput)
 	}
 }
