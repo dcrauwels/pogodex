039b9d58c3011b0a05cbae5dfeaa1659c519eba2
dcrauwels <d.crauwels@gmail.com>
2025-02-05 21:58:58 +0100

    Implemented commandMap

--- replcli/replcli.go
+++ replcli/replcli.go
@@ -2,7 +2,10 @@ package replcli
 
 import (
 	"bufio"
+	"encoding/json"
 	"fmt"
+	"io"
+	"net/http"
 	"os"
 
 	"github.com/dcrauwels/pogodex/stringutils"
@@ -17,12 +20,16 @@ type cliCommand struct {
 
 // define struct for REPL and associated function
 type REPL struct {
-	commands map[string]cliCommand
+	commands    map[string]cliCommand
+	nextURL     string // store what URL we are at when using commandMap
+	previousURL string // see above
 }
 
 func NewREPL() *REPL {
 	return &REPL{
-		commands: make(map[string]cliCommand),
+		commands:    make(map[string]cliCommand),
+		nextURL:     "",
+		previousURL: "",
 	}
 }
 
@@ -35,7 +42,7 @@ func (r *REPL) RegisterCommand(name string, description string, callback func() 
 	}
 }
 
-// first command: exit pokedex
+// first command: 'exit' pokedex
 func (r *REPL) commandExit() error {
 	fmt.Println("Closing the Pokedex... Goodbye!")
 
@@ -45,7 +52,7 @@ func (r *REPL) commandExit() error {
 	return nil
 }
 
-// second command: print help
+// second command: print 'help'
 func (r *REPL) commandHelp() error {
 	// header fluff
 	fmt.Println("Welcome to the Pokedex!\nUsage:")
@@ -62,11 +69,70 @@ func (r *REPL) commandHelp() error {
 	return nil
 }
 
+// third command: print area names on the 'map'
+func (r *REPL) commandMap() error {
+	//sanity check
+
+	// construct URL
+	var u string
+	if r.nextURL != "" {
+		u = r.nextURL // this means we have a URL stored from previous commandMap() calls
+	} else {
+		u = "https://pokeapi.co/api/v2/location-area/"
+	}
+
+	// GET request
+	res, err := http.Get(u)
+	if err != nil {
+		return fmt.Errorf("error getting request: %w", err)
+	}
+	defer res.Body.Close()
+
+	// parse response
+	body, err := io.ReadAll(res.Body)
+	if err != nil {
+		return fmt.Errorf("error parsing response: %w", err)
+	}
+
+	// unmarshal JSON to struct
+	// type locationArea
+	type locationArea struct {
+		Name string
+		Url  string
+	}
+	// type pokeapiResponse
+	type pokeapiResponse struct {
+		Count    int
+		Next     string
+		Previous *string
+		Results  []locationArea
+	}
+	var locations pokeapiResponse
+	if err = json.Unmarshal(body, &locations); err != nil {
+		return fmt.Errorf("error unmarshalling data: %w", err)
+	}
+
+	// update r.nextURL, r.previousURL
+	if locations.Previous != nil {
+		previousValue := *locations.Previous
+		r.previousURL = previousValue
+	}
+	r.nextURL = locations.Next
+
+	// print locations
+	for _, location := range locations.Results {
+		fmt.Println(location.Name)
+	}
+
+	return nil
+}
+
 // main function: open a CLI that loops until interrupt or commandExit() is called
 func (r *REPL) ReplCLI() error {
 	// register commands here
 	r.RegisterCommand("exit", "Exit the Pokedex", r.commandExit)
 	r.RegisterCommand("help", "Prints this help message", r.commandHelp)
+	r.RegisterCommand("map", "View map locations", r.commandMap)
 
 	s := bufio.NewScanner(os.Stdin)
 
